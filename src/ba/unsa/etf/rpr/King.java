443a459479dd0ab61fdfac6a11563440469d42cf
alakovic1 <alakovic1@etf.unsa.ba>
2018-11-06 22:44:56 +0100

    zadaca

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -5,17 +5,17 @@ public class King extends ChessPiece {
     public ChessPiece.Color color;
 
     public King(String position, ChessPiece.Color color) {
-        if(position.length()!=2) throw new IllegalArgumentException();
+        if (position.length() != 2) throw new IllegalArgumentException();
         if (position.charAt(0) > 'a' || position.charAt(0) < 'h') {
             Character novi = position.charAt(0);
             String manji = Character.toString(novi);
             manji = manji.toUpperCase();
             position = manji + position.charAt(1);
             if (position.charAt(0) < 'A' || position.charAt(0) > 'H') throw new IllegalArgumentException();
-            else{
-                if(position.charAt(1) < '1' || position.charAt(1) > '8') throw new IllegalArgumentException();
+            else {
+                if (position.charAt(1) < '1' || position.charAt(1) > '8') throw new IllegalArgumentException();
             }
-        } else if (position.charAt(0) > 'A' || position.charAt(0) < 'H'){
+        } else if (position.charAt(0) > 'A' || position.charAt(0) < 'H') {
             if (position.charAt(1) < '1' || position.charAt(1) > '8') throw new IllegalArgumentException();
         } else throw new IllegalArgumentException();
         this.position = position;
@@ -33,21 +33,25 @@ public class King extends ChessPiece {
     }
 
     @Override
-    public void move(String position) throws IllegalArgumentException {
-        if(position.length()!=2) throw new IllegalArgumentException();
+    public void move(String position) throws IllegalArgumentException/*, IllegalChessMoveException*/{
+        if (position.length() != 2) throw new IllegalArgumentException();
         if (position.charAt(0) > 'a' || position.charAt(0) < 'h') {
             Character novi = position.charAt(0);
             String manji = Character.toString(novi);
             manji = manji.toUpperCase();
             position = manji + position.charAt(1);
             if (position.charAt(0) < 'A' || position.charAt(0) > 'H') throw new IllegalArgumentException();
-            else{
-                if(position.charAt(1) < '1' || position.charAt(1) > '8') throw new IllegalArgumentException();
+            else {
+                if (position.charAt(1) < '1' || position.charAt(1) > '8') throw new IllegalArgumentException();
             }
-        } else if (position.charAt(0) > 'A' || position.charAt(0) < 'H'){
+        } else if (position.charAt(0) > 'A' || position.charAt(0) < 'H') {
             if (position.charAt(1) < '1' || position.charAt(1) > '8') throw new IllegalArgumentException();
         } else throw new IllegalArgumentException();
-
+        if((this.position.charAt(0)-position.charAt(0))!=1 || (this.position.charAt(0)-position.charAt(0))!=-1) {} /*throw new IllegalChessMoveException();*/
+        else if((this.position.charAt(0)==position.charAt(0)) || (this.position.charAt(0)-position.charAt(0))==1 || (this.position.charAt(0)-position.charAt(0))==-1){
+            if((this.position.charAt(1)-position.charAt(1))!=1 || (this.position.charAt(1)-position.charAt(1))!=-1){} /*throw new IllegalChessMoveException();*/
+        }
+        else this.position=position;
     }
 
 }
