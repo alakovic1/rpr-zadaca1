9c6a5c57383467e7e1aefb1a79ab549141fd4279
alakovic1 <alakovic1@etf.unsa.ba>
2018-11-06 22:17:37 +0100

    zadaca

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -5,6 +5,19 @@ public class King extends ChessPiece {
     public ChessPiece.Color color;
 
     public King(String position, ChessPiece.Color color) {
+        if(position.length()!=2) throw new IllegalArgumentException();
+        if (position.charAt(0) > 'a' || position.charAt(0) < 'h') {
+            Character novi = position.charAt(0);
+            String manji = Character.toString(novi);
+            manji = manji.toUpperCase();
+            position = manji + position.charAt(1);
+            if (position.charAt(0) < 'A' || position.charAt(0) > 'H') throw new IllegalArgumentException();
+            else{
+                if(position.charAt(1) < '1' || position.charAt(1) > '8') throw new IllegalArgumentException();
+            }
+        } else if (position.charAt(0) > 'A' || position.charAt(0) < 'H'){
+            if (position.charAt(1) < '1' || position.charAt(1) > '8') throw new IllegalArgumentException();
+        } else throw new IllegalArgumentException();
         this.position = position;
         this.color = color;
     }
@@ -21,23 +34,20 @@ public class King extends ChessPiece {
 
     @Override
     public void move(String position) throws IllegalArgumentException {
+        if(position.length()!=2) throw new IllegalArgumentException();
         if (position.charAt(0) > 'a' || position.charAt(0) < 'h') {
             Character novi = position.charAt(0);
             String manji = Character.toString(novi);
             manji = manji.toUpperCase();
             position = manji + position.charAt(1);
             if (position.charAt(0) < 'A' || position.charAt(0) > 'H') throw new IllegalArgumentException();
-            /*else{
-                if(position.charAt(1) < 1 || position.charAt(1) > 8) throw new IllegalArgumentException();
-                else {
-                    Character novi2 = position.charAt(0);
-                    String veci = Character.toString(novi2);
-                    veci = veci.toUpperCase();
-                    position = veci;
-                }
-            }*/
-        } else if (position.charAt(0) < 'A' || position.charAt(0) > 'H') throw new IllegalArgumentException();
-        if (position.charAt(1) < '1' || position.charAt(1) > '8') throw new IllegalArgumentException();
+            else{
+                if(position.charAt(1) < '1' || position.charAt(1) > '8') throw new IllegalArgumentException();
+            }
+        } else if (position.charAt(0) > 'A' || position.charAt(0) < 'H'){
+            if (position.charAt(1) < '1' || position.charAt(1) > '8') throw new IllegalArgumentException();
+        } else throw new IllegalArgumentException();
+
     }
 
 }
