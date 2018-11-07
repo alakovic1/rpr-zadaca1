c397852d16effd6c1dcfff40c371caa88e4670d3
alakovic1 <alakovic1@etf.unsa.ba>
2018-11-07 10:09:28 +0100

    zadaca

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -33,7 +33,7 @@ public class King extends ChessPiece {
     }
 
     @Override
-    public void move(String position) throws IllegalChessMoveException{
+    public void move(String position) throws IllegalChessMoveException {
         if (position.length() != 2) throw new IllegalArgumentException();
         if (position.charAt(0) > 'a' || position.charAt(0) < 'h') {
             Character novi = position.charAt(0);
@@ -47,26 +47,26 @@ public class King extends ChessPiece {
         } else if (position.charAt(0) > 'A' || position.charAt(0) < 'H') {
             if (position.charAt(1) < '1' || position.charAt(1) > '8') throw new IllegalArgumentException();
         } else throw new IllegalArgumentException();
-        if((this.position.charAt(0)==position.charAt(0))){
-            if((this.position.charAt(0)-position.charAt(0))!=0) throw new IllegalChessMoveException();
-            this.position=position;
+        if ((this.position.charAt(0) == position.charAt(0))) {
+            if ((this.position.charAt(0) - position.charAt(0)) != 0) throw new IllegalChessMoveException();
+            this.position = position;
         }
-        if(this.position.charAt(0)-position.charAt(0) > 0){
-            if((this.position.charAt(0)-position.charAt(0))!=1) throw new IllegalChessMoveException();
-            else this.position=position;
+        if (this.position.charAt(0) - position.charAt(0) > 0) {
+            if ((this.position.charAt(0) - position.charAt(0)) != 1) throw new IllegalChessMoveException();
+            else this.position = position;
         }
-        if(this.position.charAt(0)-position.charAt(0) < 0){
-            if((this.position.charAt(0)-position.charAt(0))!=-1) throw new IllegalChessMoveException();
-            else this.position=position;
+        if (this.position.charAt(0) - position.charAt(0) < 0) {
+            if ((this.position.charAt(0) - position.charAt(0)) != -1) throw new IllegalChessMoveException();
+            else this.position = position;
         }
-        if(this.position.charAt(1)-position.charAt(1) > 0){
-            if((this.position.charAt(1)-position.charAt(1))!=1) throw new IllegalChessMoveException();
-            else this.position=position;
+        if (this.position.charAt(1) - position.charAt(1) > 0) {
+            if ((this.position.charAt(1) - position.charAt(1)) != 1) throw new IllegalChessMoveException();
+            else this.position = position;
         }
-        if(this.position.charAt(1)-position.charAt(1) < 0){
-            if((this.position.charAt(1)-position.charAt(1))!=-1) throw new IllegalChessMoveException();
-            else this.position=position;
+        if (this.position.charAt(1) - position.charAt(1) < 0) {
+            if ((this.position.charAt(1) - position.charAt(1)) != -1) throw new IllegalChessMoveException();
+            else this.position = position;
         }
-        this.position=position;
+        this.position = position;
     }
 }
